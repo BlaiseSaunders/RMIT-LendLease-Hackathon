b7ea0ced7dfa5b475c27e9bc9c6a81f83ec6e32a
Boxuan Lu <547181293@qq.com>
2018-08-30 16:43:17 +1000

    onsubmit function

--- generateDoc.php
+++ generateDoc.php
@@ -1,19 +1,19 @@
 <!doctype html>
 <html>
-    <?php
-        include 'bootstrap.php';
-    ?>
-    <script type="text/javascript" src="js/DocGenerationValidation.js"></script>
+<?php
+include 'bootstrap.php';
+?>
+<script type="text/javascript" src="js/DocGenerationValidation.js"></script>
 
-    <body>
-        <div class="row text-center col-md-12">
-            <h1>What is the title?</h1>
-            <form method="POST" action="generateDocDetail.php">
-                <input type="text" id="title" name="title">
+<body>
+<div class="row text-center col-md-12">
+    <h1>What is the title?</h1>
+    <form method="POST" action="generateDocDetail.php" onsubmit="event.preventDefault(); validation()">
+        <input type="text" id="title" name="title">
 
-                <button>Cancel</button>
-                <button onclick="event.preventDefault(); validation()">Next</button>
-            </form>
-        </div>
-    </body>
+        <button>Cancel</button>
+        <button>Next</button>
+    </form>
+</div>
+</body>
 </html>
