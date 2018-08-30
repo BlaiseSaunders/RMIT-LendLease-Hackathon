6ddf72b2dc9c6898c0986da5bb5dfc3a6e306383
Boxuan Lu <547181293@qq.com>
2018-08-30 17:35:29 +1000

    second doc generation page

--- generateDocDetail.php
+++ generateDocDetail.php
@@ -6,8 +6,8 @@ include 'bootstrap.php';
 <script type="text/javascript" src="js/DocGeneration.js"></script>
 
 <body onload="getTitle()">
-<div class="container row col-md-4">
-    <h1>Title</h1><strong id="DocTitle"></strong>
+<div class="container row col-md-4 text-center">
+    <h1><?php echo $_POST['title'] ?></h1><strong id="DocTitle"></strong>
 </div>
 </body>
 </html>
