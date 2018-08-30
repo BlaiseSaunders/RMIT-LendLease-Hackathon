63b2060250307ee4155bc3f6cac1766efc83cfb4
Boxuan Lu <547181293@qq.com>
2018-08-30 17:13:47 +1000

    test onsubmit

--- generateDoc.php
+++ generateDoc.php
@@ -9,10 +9,9 @@ include 'bootstrap.php';
 <div class="row text-center col-md-12">
     <h1>What is the title?</h1>
     <form method="POST" action="generateDocDetail.php" onsubmit="return validation()">
-        <input type="text" id="title" name="title"><br>
+        <input type="text" id="title" name="title" placeholder="Please enter document title"><br>
 
-        <div class="col-md-12 text-center row">
-            <button class="btn btn-secondary">Cancel</button>
+        <div class="col-md-12 row">
             <button class="btn btn-primary" type="submit">Next</button>
         </div>
     </form>
