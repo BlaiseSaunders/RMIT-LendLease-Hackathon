a85bd0d6da873ef36303eb3a3033f8ea955e1304
Boxuan Lu <547181293@qq.com>
2018-08-30 16:04:23 +1000

    fix spelling error

--- login.php
+++ login.php
@@ -8,7 +8,7 @@
         <div class="row">
             <h1 class="container; text-center">Login</h1>
         </div>
-        <form method="POST" action="validtion.php">
+        <form method="POST" action="validation.php">
             <div class="row">
                 <p class="text-center">Username: <input type="text" id="username" name="username"></p><br>
                 <p class="text-center">Password: <input type="password" id="password" name="password"></p>
