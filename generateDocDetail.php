367a7c474dff1687707518d39ad36ab6a0b1b323
Boxuan Lu <547181293@qq.com>
2018-08-30 17:31:58 +1000

    second doc generation page

--- generateDocDetail.php
+++ generateDocDetail.php
@@ -5,8 +5,8 @@ include 'bootstrap.php';
 ?>
 <script type="text/javascript" src="js/DocGeneration.js"></script>
 
-<body onload="">
-<div class="container">
+<body onload="getTitle()">
+<div class="container row">
     <h1>Title</h1><strong id="DocTitle"></strong>
 </div>
 </body>
