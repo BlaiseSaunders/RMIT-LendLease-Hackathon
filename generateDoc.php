b1a4fbdaa92fadde4dc2a55b1454179f5ae97734
Boxuan Lu <547181293@qq.com>
2018-08-30 16:54:04 +1000

    test onsubmit

--- generateDoc.php
+++ generateDoc.php
@@ -8,11 +8,11 @@ include 'bootstrap.php';
 <body>
 <div class="row text-center col-md-12">
     <h1>What is the title?</h1>
-    <form method="POST" action="generateDocDetail.php" onsubmit="event.preventDefault(); validation()">
+    <form method="POST" action="generateDocDetail.php" onsubmit="return validation()">
         <input type="text" id="title" name="title">
 
         <button>Cancel</button>
-        <button type="submit">Next</button>
+        <button>Next</button>
     </form>
 </div>
 </body>
