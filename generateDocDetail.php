c1530f2b0fd46e65a58e8459f55c001bee18e1be
Boxuan Lu <547181293@qq.com>
2018-08-30 17:48:37 +1000

    second doc generation page

--- generateDocDetail.php
+++ generateDocDetail.php
@@ -4,10 +4,23 @@
 include 'bootstrap.php';
 ?>
 <script type="text/javascript" src="js/DocGeneration.js"></script>
+<script type="text/javascript" src="js/AddField.js"></script>
 
 <body>
-<div class="container row col-md-4 text-center">
+<div class="container row col-md-4 text-center" id="DocTitle">
     <h1>Title: </h1><strong><?php echo $_POST['title'] ?></strong>
+</div>
+<div class="container row" id="DocDetail">
+    <h3>Please select the type of format you want to add to the document: </h3>
+    <div class="dropdown">
+        <button class="dropbtn">Dropdown</button>
+        <div class="dropdown-content">
+            <a href="#" onclick="add_field(text)">Text</a>
+            <a href="#" onclick="add_field(video)">Video</a>
+            <a href="#" onclick="add_field(audio)">Audio</a>
+        </div>
+    </div>
+    <div id="form_zone" />
 </div>
 </body>
 </html>
