d8ab450d24a8c300db826d4ef0166a13270963b6
David_Roth <derpydeponson@gmail.com>
2018-08-31 16:34:53 +1000

    :^)

--- src/php/getDocument.php
+++ src/php/getDocument.php
@@ -4,9 +4,13 @@ require "sql_header.php";
 
 $docuGet = "SELECT documentID, name FROM Document ";
 
+
+$feed = true;
+
 if (isset($_GET['docunum']) == TRUE)
 {
 	$docuGet .= "WHERE documentID=".$_GET['docunum'];
+	$feed = false;
 	//echo "DOCUNUM <br/>$docuGet<br/>";
 }
 
@@ -19,10 +23,15 @@ if ($result->num_rows > 0)
 {
 	while ($row = $result->fetch_assoc())
 	{
+		$docuID = $row['documentID'];
+		if ($feed)
+			echo "<a href='getDocument.php?docunum=$docuID'>";
 		echo "<h1>Document: ".$row['name']."</h1><br/>";
+		if ($feed)
+			echo "</a>";
 		$documentID = $row['documentID'];
 		$docres = $conn->query($docuDataGet.$documentID);
-		if ($docres->num_rows > 0)
+		if ($docres->num_rows > 0 && $feed == false)
 		{
 			while ($dataRow = $docres->fetch_assoc())
 			{
