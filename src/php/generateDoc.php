752cd8399efc79d6b79adb893fdc489fb8dd0f73
BoxHezi <547181293@qq.com>
2018-08-31 14:09:31 +1000

    fix format

--- src/php/generateDoc.php
+++ src/php/generateDoc.php
@@ -10,10 +10,10 @@ include 'bootstrap.php';
 include_once 'pageHeader.php';
 ?>
 <div class="row text-center col-md-12">
-    <h1>What is the title?</h1>
+    <h1>Title of the Document</h1>
     <form method="POST" action="generateDocDetail.php" onsubmit="return validation()">
-        <input type="text" id="title" name="title" placeholder="Please enter document title"><br>
-
+        <input type="text" id="title" name="title" size="40" placeholder="Please enter document title"/><br>
+        <hr style="visibility: hidden">
         <div class="col-md-12 row">
             <button class="btn btn-primary" type="submit">Next</button>
         </div>
