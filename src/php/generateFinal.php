6ffdbc98d6efb0e08b2b6f48addd0b76186db479
David_Roth <derpydeponson@gmail.com>
2018-08-31 13:32:04 +1000

    :^)

--- src/php/generateFinal.php
+++ src/php/generateFinal.php
@@ -15,8 +15,20 @@ $documentType = "Smart Document...";
 echo "document title: ".$documentName;
 
 
+if ($conn->query($docuCreate."( ".$documentName.", ".$documentType." )") == TRUE)
+{    
+	$last_id = $conn->insert_id;
+	echo "New record created successfully. Last inserted ID is: " . $last_id;
+} 
+else
+    echo "Error: " . $conn->error;
+
+
+
 foreach ($_POST['text_field'] as $text)
+{
 	echo "<br> TETX: $text<br>";
+}
 
 
 
