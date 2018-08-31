0c38fde581f261c6a912db81e6e68b50fc66efa3
David_Roth <derpydeponson@gmail.com>
2018-08-31 22:01:46 +1000

    :^)

--- src/php/generateFinal.php
+++ src/php/generateFinal.php
@@ -12,13 +12,13 @@ $documentType = "1";
 
 
 
-echo "document title: ".$documentName;
+//echo "document title: ".$documentName;
 
 
 if ($conn->query($docuCreate."( '".$documentName."', '".$documentType."' )") == TRUE)
 {    
 	$last_id = $conn->insert_id;
-	echo "New record created successfully. Last inserted ID is: " . $last_id;
+//	echo "New record created successfully. Last inserted ID is: " . $last_id;
 } 
 else
     echo "Error: " . $conn->error;
@@ -39,25 +39,26 @@ $text_array[] = "<h4>Conclusion: ".$_POST['conclusionDoc']."</h4>";
 $dataLocation = "/srv/files/";
 foreach ($text_array as $text)
 {
-	echo "<br> TEXT: $text<br>";
+	//echo "<br> TEXT: $text<br>";
 	$hash = hash("md5", $text);
 	$thisDataLocation = $dataLocation.$hash;
 	file_put_contents($thisDataLocation, $text);
 	if ($conn->query($docuDataInsert."( '".$documentID."', 0, '".$thisDataLocation."' )") == TRUE)
 	{    
 		$last_id = $conn->insert_id;
-		echo "New record created successfully. Hash: ".$hash."Last inserted ID is: " . $last_id;
+		//echo "New record created successfully. Hash: ".$hash."Last inserted ID is: " . $last_id;
 	} 
 	else
-		echo "Error: " . $conn->error;
+		;
+	//	echo "Error: " . $conn->error;
 }
 
 
 
-echo "FILES: <br/>";
-var_dump($_FILES);
-echo "POST: <br/>";
-var_dump($_POST);
+//echo "FILES: <br/>";
+//var_dump($_FILES);
+//echo "POST: <br/>";
+//var_dump($_POST);
 
 $fileDataLocation = "/srv/hack/srv/files/";
 foreach ($_FILES['file_upload']['tmp_name'] as $file) // TODO: MAKE THIS
@@ -67,21 +68,22 @@ foreach ($_FILES['file_upload']['tmp_name'] as $file) // TODO: MAKE THIS
 	$thisDataLocation = $fileDataLocation.$hash;
 	
 	// MOVE FILE TO LOCATION
-	echo "File uploaded to: ".$file."<br/>";
+//	echo "File uploaded to: ".$file."<br/>";
 	move_uploaded_file($file, $thisDataLocation);
-	echo "Moved file to $thisDataLocation<br/>";
+//	echo "Moved file to $thisDataLocation<br/>";
 
 	if ($conn->query($docuDataInsert."( '".$documentID."', 1, '".$dataLocation.$hash."' )") == TRUE)
 	{    
 		$last_id = $conn->insert_id;
-		echo "New record created and file uploaded successfully. Hash: ".$hash."Last inserted ID is: " . $last_id;
+//		echo "New record created and file uploaded successfully. Hash: ".$hash."Last inserted ID is: " . $last_id;
 	} 
 	else
-		echo "Error: " . $conn->error;
+		;
+//		echo "Error: " . $conn->error;
 }
 
 
 
-echo "<h1>Document Generated!! (probably)</h1>";
+echo "<h1>Document Generated</h1>";
 
 ?>
