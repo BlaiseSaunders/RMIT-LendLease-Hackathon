256ffea8c30d4d3629eea3feef6f490d8ffe3234
BoxHezi <547181293@qq.com>
2018-08-31 11:57:30 +1000

    add leadlease format to doc generate page

--- src/php/generateDocDetail.php
+++ src/php/generateDocDetail.php
@@ -14,40 +14,52 @@ include 'bootstrap.php';
 include_once 'pageHeader.php';
 ?>
 <div class="container">
-<div class="row col-md-4" id="DocTitle">
-    <h1 class="title-float-left">Title: </h1>
-    <h2 class="title-float-right"><strong><?php echo $_POST['title'] ?></strong></h2>
-    <hr class="float-clear">
-</div>
+    <div class="row col-md-12" id="DocTitle">
+        <h1 class="title-float-left">Title: </h1>
+        <h2 class="title-float-right"><strong><?php echo $_POST['title'] ?></strong></h2>
+        <hr class="float-clear separate-line">
+    </div>
 </div>
 <br>
 <div class="container">
-<div class="container row" id="DocDetail">
-    <h3>Please select the type of format you want to add to the document: </h3>
-    <div class="dropdown">
-        <button class="dropbtn">Dropdown</button>
-        <div class="dropdown-content">
-            <a href="#" onclick="add_field('text')">Text</a>
-            <a href="#" onclick="add_field('video')">Video</a>
-            <a href="#" onclick="add_field('audio')">Audio</a>
+    <div id="docDetail">
+        <div class="row col-md-12">
+            <form action="generateFinal.php" method="post">
+                <div class="container" id="docIdentifier">
+                    <p>Incident circumstance: </p><input type="text" id="incidentCircumstance"
+                                                         name="incidentCircumstance"/>
+                    <p>Incident No.: </p><input type="text" id="incidentDate" name="incidentDate">
+                    <p>Issued by: </p><input type="text" id="docAuthor" name="docAuthor">
+                    <p>Incident Date: </p><input type="date" id="incidentDate" name="incidentDate">
+                </div>
+                <hr class="separate-line">
+                <h3>Please select the type of format you want to add to the document: </h3>
+                <div class="dropdown">
+                    <button class="dropbtn">Dropdown</button>
+                    <div class="dropdown-content">
+                        <a href="#" onclick="add_field('text')">Text</a>
+                        <a href="#" onclick="add_field('video')">Video</a>
+                        <a href="#" onclick="add_field('audio')">Audio</a>
+                    </div>
+                </div>
+
+                <input type="hidden" value="<?php echo $_POST['title'] ?>"/>
+
+                <div id="form_zone">
+
+                </div>
+
+                <hr class="separate-line">
+
+                <div id="conclusion">
+                    <h5>Conclusion</h5>
+                    <textarea>
+            </textarea>
+                </div>
+                <button class="btn btn-primary">Confirm</button>
+            </form>
         </div>
     </div>
-    <div class="row col-md-12">
-        <form action="generateFinal.php" method="post">
-            <input type="hidden" value="<?php echo $_POST['title'] ?>"/>
-            <div id="form_zone">
-
-            </div>
-            <div id="conclusion">
-                <h5>Conclusion</h5>
-                <textarea>
-
-            </textarea>
-            </div>
-            <button class="btn btn-primary">Confirm</button>
-        </form>
-    </div>
-</div>
 </div>
 </body>
 </html>
