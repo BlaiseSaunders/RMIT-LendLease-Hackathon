6e898a26bd308bdedf5ec3ace730a0d6d6deaf87
David_Roth <derpydeponson@gmail.com>
2018-08-31 20:39:56 +1000

    :^)

--- src/php/addComment.php
+++ src/php/addComment.php
@@ -7,7 +7,7 @@ $commentCreate = "INSERT INTO Comment (userID, documentID, comment) VALUES ";
 
 
 $commentText = $_POST['text'];
-$documentID = $_POST['docid'];
+$documentID = $_POST['documentID'];
 
 
 echo "Comment text: ".$commentText."<br/>";
