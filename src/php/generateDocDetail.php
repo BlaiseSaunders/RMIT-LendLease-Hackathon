b9e3bf6fc756173103265b642e62e0ef01ca2156
BoxHezi <547181293@qq.com>
2018-08-31 13:50:25 +1000

    document list page format

--- src/php/generateDocDetail.php
+++ src/php/generateDocDetail.php
@@ -1,14 +1,15 @@
 <!doctype html>
 <html>
-<?php
-include 'bootstrap.php';
-?>
+<head>
+    <?php
+    include 'bootstrap.php';
+    ?>
 
-<script type="text/javascript" src="../js/DocGeneration.js"></script>
-<script type="text/javascript" src="../js/AddField.js"></script>
-<link rel="stylesheet" type="text/css" href="../css/dropdown.css">
-<link rel="stylesheet" type="text/css" href="../css/docGeneration.css">
-
+    <script type="text/javascript" src="../js/DocGeneration.js"></script>
+    <script type="text/javascript" src="../js/AddField.js"></script>
+    <link rel="stylesheet" type="text/css" href="../css/dropdown.css">
+    <link rel="stylesheet" type="text/css" href="../css/docGeneration.css">
+</head>
 <body>
 <?php
 include_once 'pageHeader.php';
@@ -27,10 +28,13 @@ include_once 'pageHeader.php';
             <form action="generateFinal.php" method="post">
                 <div class="container" id="docIdentifier">
                     <p>Incident circumstance: </p><input type="text" id="incidentCircumstance"
-                                                         name="incidentCircumstance" placeholder="Incident circumstance..."/>
-                    <p>Incident No.: </p><input type="text" id="incidentDate" name="incidentDate" placeholder="Incident No."/>
+                                                         name="incidentCircumstance"
+                                                         placeholder="Incident circumstance..."/>
+                    <p>Incident No.: </p><input type="text" id="incidentDate" name="incidentDate"
+                                                placeholder="Incident No."/>
                     <p>Issued by: </p><input type="text" id="docAuthor" name="docAuthor" placeholder="Author..."/>
-                    <p>Incident Date: </p><input type="date" id="incidentDate" name="incidentDate" placeholder="Incident date..."/>
+                    <p>Incident Date: </p><input type="date" id="incidentDate" name="incidentDate"
+                                                 placeholder="Incident date..."/>
                 </div>
                 <hr class="separate-line">
                 <div class="container" id="docLearning">
